7b21cb47aacb8ac2d45a45103cd3516585519c10
ChaitaliGhosh23 <65826084+ChaitaliGhosh23@users.noreply.github.com>
2020-05-24 17:13:23 +0530

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,16 @@
 import random
 def main():
-  #print("Keep it logically awesome.")
+  print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
-  last=len(quotes)-1
-  rnd=random.randint(0,last)
+  #f = open("quotes.txt")
+  #quotes = f.readlines()
+  #f.close()
+  #last=len(quotes)-1
+  #rnd=random.randint(0,last)
 
-  print(quotes[rnd])
+  #print(quotes[rnd])
+  print("""My name is Chaitali Ghosh,
+  I am from Bongaon""")
 
 if __name__== "__main__":
   main()
